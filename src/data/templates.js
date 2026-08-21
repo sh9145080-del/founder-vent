6e477951646285369926d859df9b25dfa05ef7c8
sh9145080-del <sh9145080@gmail.com>
2026-08-21 20:04:12 +0600

    Update templates.js

--- src/data/templates.js
+++ src/data/templates.js
@@ -1,1 +1,27 @@
-
+export const templates = [
+  {
+    id: "template-1",
+    name: "Template 1",
+    description: "Landing page template 1",
+  },
+  {
+    id: "template-2",
+    name: "Template 2",
+    description: "Landing page template 2",
+  },
+  {
+    id: "template-3",
+    name: "Template 3",
+    description: "Landing page template 3",
+  },
+  {
+    id: "template-4",
+    name: "Template 4",
+    description: "Landing page template 4",
+  },
+  {
+    id: "template-5",
+    name: "Template 5",
+    description: "Landing page template 5",
+  },
+];
